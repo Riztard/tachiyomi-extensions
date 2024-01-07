38c34ae815fc17689393433c9e56122bda43f22f
Riztard Lanthorn <16263232+Riztard@users.noreply.github.com>
2024-01-07 15:34:13 +0700

    roundabout

--- multisrc/overrides/madara/shinigami/src/Shinigami.kt
+++ multisrc/overrides/madara/shinigami/src/Shinigami.kt
@@ -35,7 +35,7 @@ class Shinigami : Madara("Shinigami", "https://shinigami.moe", "id") {
 
     private val encodedString = "AAA AaAAAAH QAAAB0 AAAAcA AAAHMAA AA6AAA ALwAAAC8AA " + "AB0AAAAYQA AAGM AAADoAAAAaQAAAH kAAABvAA AAbQAAA  GkAAABvAAAA cgAAAGcAAAAuAAA AZwAAAGk  " + "AAAB0AAAA aAAAAHUAA ABiAAAALgAAAGkAA ABvAAAAL   wAAAHUAAABzA AAAZQAAAHIAAAAtA AAAYQAAAGcA " + "AABlyAtAAAbgA AAHQAAAB6AAAA LwAAAHUAAA  BcAAAAZQ AAAHIAAAAtAAA AYQAAAGcAAABl AAAAbgAA  AHQAAAB6AAAALgAAAG" + "     oAhAntUAABzAA AAbwAAAG4="
 
-    private val tachiUaUrl = Base64.decode(encodedString.replace(" ", "").replace("DoA", "BoA").replace("GoAhAntU", "GoA").replace("BlyAt", "BlA").replace("BcA", "BzA"), Base64.DEFAULT).toString(Charsets.UTF_32).replace("z", "s")
+    private val tachiUaUrl = Base64.decode(encodedString.replace("\\s".toRegex(), "").replace("DoA", "BoA").replace("GoAhAntU", "GoA").replace("BlyAt", "BlA").replace("BcA", "BzA"), Base64.DEFAULT).toString(Charsets.UTF_32).replace("z", "s")
 
     private var secChUaMP: List<String>? = null
     private var userAgent: String? = null
