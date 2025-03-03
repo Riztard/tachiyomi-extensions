ade9be7da358e1696386bb2261d3c5af5a21f36c
Luqman <16263232+Riztard@users.noreply.github.com>
2025-03-03 22:18:21 +0700

    Shinigami X: fix decimal chapter

--- src/id/shinigamix/src/eu/kanade/tachiyomi/extension/id/shinigamix/ShinigamiX.kt
+++ src/id/shinigamix/src/eu/kanade/tachiyomi/extension/id/shinigamix/ShinigamiX.kt
@@ -224,8 +224,8 @@ class ShinigamiX : ConfigurableSource, HttpSource() {
     }
 
     private fun chapterFromObject(obj: ShinigamiXChapterListDataDto): SChapter = SChapter.create().apply {
-        date_upload = obj.date.toDate() ?: 0
-        name = "Chapter ${obj.name} ${obj.title}"
+        date_upload = obj.date.toDate()
+        name = "Chapter ${obj.name.toString().replace(".0","")} ${obj.title}"
         url = "$apiUrl/$API_BASE_PATH/chapter/detail/" + obj.chapterId
     }
 
